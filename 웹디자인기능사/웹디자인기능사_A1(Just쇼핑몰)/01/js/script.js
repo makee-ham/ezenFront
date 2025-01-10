8732eaf459a2f57d03746de3c66f5bcf0360f65e
Makee Ham <ina0312@naver.com>
2025-01-10 15:08:08 +0900

    250110

---
+++ 웹디자인기능사/웹디자인기능사_A1(Just쇼핑몰)/01/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+$(document).ready(function(){
+	// menu
+	$(".navi > li").mouseover(function(){
+		$(".submenu").stop().slideDown();
+	});
+	$(".navi > li").mouseout(function(){
+		$(".submenu").stop().slideUp();
+	});
+	
+	// tabmenu
+	$(".tabmenu > ul > li > a").on("click", function(){
+		$(this).parent().addClass('active').siblings().removeClass('active');
+	});
+	
+	// image slide
+	setInterval(function(){
+		$(".slidelist").animate({"marginTop" : "-300px"}, function(){
+			$(".slidelist a:first").appendTo(".slidelist");
+			$(".slidelist").css("margin-top", "0");
+		});
+	} ,3000);
+});
