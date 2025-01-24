ac6d798741748cf0b6ea1a168a80045eb2c0e7fb
Makee Ham <ina0312@naver.com>
2025-01-24 13:00:56 +0900

    250124

---
+++ 은행계좌관리 앱/script.js
@@ -0,0 +1,53 @@
+let cust = [['A101', '김삿갓', '111-111-1111', true, 0, 1000000], 
+			['A102', '정선달', '111-111-2222', false, 0, 500000],
+			['A103', '윤나라', '111-111-3333', false, 0, 300000]]
+
+function regist(frm) {
+	const element = [frm.cNo.value, frm.cName.value, frm.cNumber.value, frm.yesNo.checked, 0, Number(frm.cLimit.value)]
+	cust.push(element)
+	console.log(cust)
+}
+function inStock(frm) {
+	var num = frm.cNo.value
+	for(i=0; i<cust.length; i++){
+		if(cust[i][0] == num) {
+			break;
+		}
+	}
+	if(i<cust.length){
+		cust[i][4] += Number(frm.incost.value)
+	}else{
+		alert("해당 고객이 존재하지 않습니다")
+	}
+	console.log(cust)
+}
+function outStock(frm) {
+	var num = frm.cNo.value
+	for(i=0; i<cust.length; i++){
+		if(cust[i][0] == num) {
+			break;
+		}
+	}
+	if(cust[i][4]<Number(frm.outcost.value) && cust[i][3] == false){
+		alert("잔액이 부족하여 인출할 수 없습니다")
+	}else{
+		cust[i][4] -= Number(frm.outcost.value)
+	}
+	console.log(cust)
+}
+function appr(frm){
+	var num = frm.cNo.value
+	for(i=0; i<cust.length; i++){
+		if(cust[i][0] == num) {
+			break;
+		}
+	}
+	if(cust[i][5]<Number(frm.approval.value)){
+		alert("한도초과")
+	}else{
+		apprt = cust[i][5] - Number(frm.approval.value)
+		cust[i][5] -= Number(frm.approval.value)
+		alert("결재액"+frm.approval.value+"\n"
+			+"남은 한도"+cust[i][5])
+	}
+}
