bf4424bc58c59cf5bcef1d7b7036ac09b0ffed42
Makee Ham <ina0312@naver.com>
2025-02-11 20:40:36 +0900

    250211

--- myPractice/minesweeper/script.js
+++ myPractice/minesweeper/script.js
@@ -4,76 +4,39 @@
 const rows = document.querySelectorAll(".row");
 const cols = document.querySelectorAll(".col");
 
-/////////////////////////////////////////////////////////
-
-// 처음으로 클릭한 .col의 index를 불러오기
-// // 일단 .col 안의 개성 없는 li들에 index라는 개성 부여하기...!
-// for (let i = 0; i < cols.length; i++) {
-//   cols[i].classList.add(`${i}`);
-// }
-// // 개성이 생긴 li(.col)들에게 클릭 이벤트 리스너 전부 등록
-// // https://gahyun-web-diary.tistory.com/294
-// cols.forEach((event) => {
-//   event.addEventListener("click", function () {
-//     // ... 어라 jQuery에 index()라는 게 있네? 이걸 바닐라로 구현해보자
-//   });
-// });
-
-/////////////////////////////////////////////////////////
-
-// 재도전! '처음으로' 클릭한 .col의 index를 불러오기
-
-// 매 회기 처음으로 클릭한 .col의 좌표를 기억하는 변수
-let firstClick = [];
-// 메모: 좌표는 [row, col] 형태로 저장되는데, 이거로 li를 특정할 수 있나?
-
-// 대상 element의 index 불러오는 함수, getElmIndex
-// https://stackoverflow.com/questions/13658021/jquery-index-in-vanilla-javascript
-const getElmIndex = (elm) => [...elm.parentNode.children].indexOf(elm);
-
-// 메모: 아래를 새 게임의 최초 필드 좌클릭 순간에 한 번만 실행!!!
-// (firstClick 초기화 잊지 말자)
-
-// firstClick에 [row, col] 좌표를 저장해주는 작업
-cols.forEach((event) => {
-  event.addEventListener(
-    "click",
-    function () {
-      rows.forEach((event) => {
-        event.addEventListener(
-          "click",
-          function () {
-            firstClick.push(getElmIndex(event));
-          },
-          // row와 col 각각에 once:true 적용으로
-          // 매트릭스? 필드 전체에 단 한 번만 클릭 이벤트 발생하게 함
-          // https://velog.io/@khd/%ED%95%9C%EB%B2%88%EB%A7%8C-%EC%8B%A4%ED%96%89%ED%95%98%EB%8A%94-%ED%95%A8%EC%88%98-oncetrue-X
-          { once: true }
-        );
-      });
-      firstClick.push(getElmIndex(event));
-    },
-    { once: true }
-  );
-});
-
-//////////////////////////////////////////////////
-
-// 지뢰에 해당되는 index 저장용 배열
+// 선언
+let firstClickIndex = 0;
 let randomMines = [];
+let isFirstClick = false;
+let aroundClick = [];
+const aroundGap = [-10, -9, -8, -1, 1, 8, 9, 10];
+let mineIsNear = [];
 
-// 지뢰를 세팅하는 함수
-// 목표: cols 배열의 요소 중 랜덤으로 10개의 요소에 지뢰 설치해야 함!
-// 0부터 cols.length - 1까지의 수 중 10개를 랜덤으로 골라 기억하는 함수 만들기
-// (이 함수의 발동 타이밍은 아무 '.col'이나 눌렀을 때)
+// 초기화 함수
+const settingNewGame = function () {
+  // 1. 대시보드 초기화
+  document.querySelector(".bombCount").innerHTML = "010";
+  document.querySelector(".timeCount").innerHTML = "000";
+  document.querySelector(".newGame > img").src = "img/smile.png";
+  // 2. 필드 초기화
+  cols.forEach((element) => (element.className = ""));
+  cols.forEach((element) => (element.className = "col"));
+  // 3. 첫 클릭 관련 초기화
+  firstClickIndex = 0;
+  randomMines = [];
+  isFirstClick = false;
+};
 
-// 메모: firstClick의 좌표 제외하고 지뢰 설치해야 함!!!
+// 첫 클릭 index 저장 함수
+const getFirstIndex = (index) => (firstClickIndex = index);
+
+// 지뢰 세팅 함수
 const getRandomIndex = () => {
   let temp = [];
   let randomIndex = 0;
   for (let i = 0; i < 9; ) {
     randomIndex = Math.floor(Math.random() * cols.length);
-    if (temp.includes(randomIndex)) {
+    if (temp.includes(randomIndex) || randomIndex === firstClickIndex) {
       i--;
     } else {
       i++;
@@ -83,15 +46,77 @@ const getRandomIndex = () => {
   randomMines = [...temp];
 };
 
-// 메모: 지뢰로 설정된 요소(randomMines에 있는 index 가진 요소들)를 좌클릭 시 '.mine' 클래스 추가
-// .mine은 좌클릭 시 게임오버+전체 .mine 위치 공개
-// https://heojju.tistory.com/14
+// 클릭한 지점 기준으로 8방향 호출 및 숫자 세팅 함수
+const mineDetector = (col, index) => {
+  // 임시값에 index + aroundGap 계산 받고 배열에 넣어주기
+  aroundGap.forEach((gap) => {
+    if (index + gap >= 0 && index + gap <= 80) {
+      aroundClick.push(index + gap);
+    }
+  });
 
-// 메모: <타이머>
-// [새 게임 최초 필드 좌클릭 시]
-// ㄴ타이머 시작
-// [타이머 종료 시점]
-// ㄴ게임 오버
-// ㄴ게임 클리어
-// [타이머 초기화 시점]
-// ㄴ새 게임 클릭
+  // opened 클래스 추가
+  cols[index].classList.add("opened");
+
+  // 주변에 지뢰가 있는지 여부 확인하기
+  // https://velog.io/@ireneeming/JavaScript-%EB%91%90-%EB%B0%B0%EC%97%B4-%EB%B9%84%EA%B5%90%ED%95%98%EA%B8%B0-filter
+  mineIsNear = aroundClick.filter((element) => randomMines.includes(element));
+
+  // 지뢰가 근처에 있는 경우
+  if (mineIsNear.length !== 0) {
+    // 숫자 써주기
+    col.innerHTML = `${mineIsNear.length}`;
+    // aroundClick, mineIsNear 초기화
+    aroundClick = [];
+    mineIsNear = [];
+    // 함수 종료
+    return;
+
+    // 지뢰가 근처에 없는 경우
+  } else {
+    // 열어제쳐~~
+    aroundClick.forEach((noMineCell, noMineIndex) => {
+      cols[noMineCell].classList.add("opened");
+      // aroundClick에 있는 8방향 8개의 셀에도 mineDetector 사용(재귀)
+      mineDetector(noMineCell, noMineIndex);
+    });
+    // aroundClick, mineIsNear 초기화
+    aroundClick = [];
+    mineIsNear = [];
+  }
+};
+
+// 좌클릭 이벤트
+cols.forEach((col, index) => {
+  col.addEventListener("click", function () {
+    // '첫 클릭' 때에만 일어나야 하는 것
+    if (!isFirstClick) {
+      // 1. 첫 클릭 인지시키기
+      isFirstClick = true;
+      // 2. 첫 클릭 index 저장
+      getFirstIndex(index);
+      // 3. 지뢰 배치
+      getRandomIndex();
+      // 5. 타이머 시작
+    } else {
+      // 4. 숫자(8방향 지뢰 수) 배치
+      mineDetector(col, index);
+    }
+    // 좌클릭 시 일어나는 것 (기본)
+    // <지뢰 셀이 아닐 경우>
+    // 1. 클릭한 지점 오픈
+    // 2. 주변 셀 오픈(숫자 나올 때까지)
+    // <지뢰 셀일 경우 = 'GAME OVER'>
+    // 1. 전체 지뢰 위치 공개
+    // 2. 필드 클릭 불가능
+    // 3. 타이머 stop
+    // 4. .newGame 버튼의 표정 바뀌기
+
+    // <지뢰가 아닌 모든 셀을 열었을 때 = 지뢰 셀만 남았을 때 = GAME CLEAR>
+    // 1. 필드 클릭 불가능
+    // 2. 타이머 stop
+    // 3. .newGame 버튼의 표정 바뀌기
+  });
+});
+
+// 메모: 이미 .opened 있는 곳은 재클릭 불가
